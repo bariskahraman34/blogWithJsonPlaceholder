38e84b5525b2d1ada5cc7b74f2c7ff141f8201c9
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-01 17:30:57 +0300

    Search için işlemler tamamlandı. items ile bütün sayfa başlıkları seçildi. seçilen başlıkların innerTextleri lowerCase yapıldı sonrasında girilen input ile eşleşen bir title var ise onların içinde bulunduğu cardContainer'ın style ı block elemanı yapıldı diğerleri ise gizlendi.

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,5 +2,20 @@ const searchInput = document.querySelector('.search-input');
 
 searchInput.addEventListener('input',function(){
     const searchTerm = searchInput.value.toLowerCase();
-    console.log(searchTerm);
+    return searchData(searchTerm);
 })
+
+function searchData(searchTerm){
+    const items = document.querySelectorAll('.h3-name');
+    const cardContainer = document.querySelectorAll('.card-container');
+    for (let i = 0 ; i < cardContainer.length ; i++) {
+        const title = items[i].innerText.toLowerCase();
+        
+        if(title.includes(searchTerm)){
+            cardContainer[i].style.display = "block";
+        }else{
+            cardContainer[i].style.display = "none";
+        }
+    }
+}
+
