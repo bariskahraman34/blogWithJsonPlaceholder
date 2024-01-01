74502c7e13296b60f7845aba7be60a14a9ad81a0
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-01 18:33:57 +0300

    Lorem picsum ile daha önceden rasgele resimler alınıyordu. Şu an loremPicsum un api si kullanılarak veriler alınıyor ve her bir card'a farklı resim atanıyor. Böylece resimler ve içerikler farklı apilerden olmasına rağmen sürekli aynı şekilde geliyor.

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,8 +12,13 @@ async function fetchComments(){
     const response = await fetch('https://jsonplaceholder.typicode.com/comments');
     const data = await response.json();
     return data;
-} 
+}
 
+async function fetchLoremPicsum(){
+    const response = await fetch('https://picsum.photos/v2/list?limit=100');
+    const data = await response.json();
+    return data;
+}
 let currentPage = 1;
 let itemsPerPage = 9;
 
@@ -29,8 +34,6 @@ async function pagination(){
     bindPaginationLi();
 }
 
-
-
 function bindPaginationLi(){
     const paginationLiElements = document.querySelectorAll('.paginationLi');
     for (const paginationLi of paginationLiElements) {
@@ -40,26 +43,29 @@ function bindPaginationLi(){
             }
             paginationLi.classList.add('active');
             currentPage = Number(paginationLi.innerHTML);
+            if(currentPage == 1){
+                counter = 0;
+            }else{
+                counter = (currentPage - 1)*itemsPerPage;
+            }
             searchInput.value = "";
             getPosts();
         })
     }
 }
-
-
-
 let counter = 0;
 async function getPosts(){
     const posts = await fetchPosts();
     const comments = await fetchComments();
+    const pictures = await fetchLoremPicsum();
     let commentDivCounter = 0;
     cardItems.innerHTML = "";
     for (const post of posts.slice((currentPage-1)*itemsPerPage,(currentPage-1)*itemsPerPage+itemsPerPage)) {
         cardItems.innerHTML +=
         `
         <div class="card-container">
-            <div class="img-container">
-                <img src="https://picsum.photos/380/200?random=${counter}">
+            <div class="img-container" id="imgContainer-${commentDivCounter}">
+                
             </div>
             <div class="blog-name">
                 <h3 class="h3-name">${post.title}</h3>
@@ -69,8 +75,8 @@ async function getPosts(){
                     <button class="close-btn">
                         <i class="fa-regular fa-circle-xmark fa-2x"></i>
                     </button>
-                    <div class="dialog-img-container">
-                        <img src="https://picsum.photos/380/200?random=${counter}">
+                    <div class="dialog-img-container" id="dialogImgContainer-${commentDivCounter}">
+                        
                     </div>
                     <h3 class="dialog-title">${post.title}</h3>
                     <p class="dialog-body">${post.body}</p>
@@ -86,7 +92,7 @@ async function getPosts(){
             </div>
         </div>
         `
-        counter++;
+        counter ++;
         let commentImageCounter = 0;
         for(const comment of comments){
             const comments = document.querySelector(`#comment-${commentDivCounter}`);
@@ -108,6 +114,21 @@ async function getPosts(){
                 `
             }
             commentImageCounter++;
+        }
+
+        for (const picture of pictures) {
+            const imgContainer = document.querySelector(`#imgContainer-${commentDivCounter}`);
+            const dialogImgContainer = document.querySelector(`#dialogImgContainer-${commentDivCounter}`);
+            if(post.id == picture.id){
+                imgContainer.innerHTML += 
+                `
+                <img src="${picture.download_url}" width="380" height="200">
+                `
+                dialogImgContainer.innerHTML +=
+                `
+                <img src="${picture.download_url}" width="380" height="200">
+                `
+            }
         }
         commentDivCounter++;
     }
@@ -145,5 +166,6 @@ function bindCloseBtns(){
         })
     }
 }
+
 pagination();
 getPosts();
