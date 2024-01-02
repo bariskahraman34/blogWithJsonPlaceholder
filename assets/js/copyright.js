82ce7d9f8aee6e9a0111a288f65955e4de75a38a
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-02 03:53:11 +0300

    Copyright yazısı eklendi. npmjs 'te sunulan snowflakes isminde obje ile bir kar yağdırma efekti verildi. bu efekti kapatma özelliği için bir input eklendi ve css ve js işlemleri tamamlandı.

---
+++ assets/js/copyright.js
@@ -0,0 +1,7 @@
+const copyright = document.querySelector('#copyright');
+const year = new Date().getFullYear();
+
+copyright.innerHTML = 
+`
+<h3>Powered by Barış Kahraman, © Copyright ${year} all rights reserved.</h3>
+`
